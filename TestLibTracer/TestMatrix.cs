3df05e30ea4cb6ab9e375896dcf826c9a9e9a68a
Grahame White <4whitg16@solent.ac.uk>
2021-10-30 20:40:03 +0100

    Added some utility functions to the matric class

---
+++ TestLibTracer/TestMatrix.cs
@@ -0,0 +1,237 @@
+﻿using System;
+
+using libTracer;
+
+using NUnit.Framework;
+
+namespace TestLibTracer
+{
+    internal class TestMatrix
+    {
+        private const Int32 ROW_1 = 1;
+        private const Int32 COL_1 = 1;
+
+        private TMatrix _matrix;
+
+        [Test]
+        public void Columns_Returns_NumberOfColumns()
+        {
+            _matrix = new TMatrix(
+                new[,] { 
+                    { 1f, 2f, 3f }, 
+                    { 3f, 4f, 5f } });
+            Assert.That(_matrix.Columns, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Columns_Returns_NumberOfRows()
+        {
+            _matrix = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            Assert.That(_matrix.Rows, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Indexer_ReturnsValue_FromSpecifiedRowAndCol()
+        {
+            _matrix = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+
+
+            Assert.That(_matrix[COL_1, ROW_1], Is.EqualTo(4f));
+        }
+
+        [Test]
+        public void Equals_ReturnsTrue_WhenMatrixContainsSamesValuesInSamePositions()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            var matrix2 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+
+            Assert.That(matrix1.Equals(matrix2), Is.True);
+        }
+
+        [Test]
+        public void Equals_ReturnsFalse_WhenMatrixDoesNotContainSamesValuesInSamePositions()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            var matrix2 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 6f } });
+
+            Assert.That(matrix1.Equals(matrix2), Is.False);
+        }
+
+        [Test]
+        public void Equals_ReturnsFalse_WhenComparedAgainstNull()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            TMatrix matrix2 = null;
+
+            Assert.That(matrix1.Equals(matrix2), Is.False);
+        }
+
+        [Test]
+        public void Equals_ReturnsTrue_WhenComparedAgainstReferenceToSelf()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            TMatrix matrix2 = matrix1;
+
+            Assert.That(matrix1.Equals(matrix2), Is.True);
+        }
+
+        [Test]
+        public void Equals_ReturnsFalse_WhenMarticesHaveDifferentNumberOfDimensions()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            var matrix2 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f },
+                    { 6f, 7f, 8f }
+                });
+
+            Assert.That(matrix1.Equals(matrix2), Is.False);
+        }
+
+        [Test]
+        public void Equals_ReturnsFalse_WhenComparedAgainstNullObject()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            Object matrix2 = null;
+
+            Assert.That(matrix1.Equals(matrix2), Is.False);
+        }
+
+        [Test]
+        public void Equals_ReturnsTrue_WhenComparedAgainstObjectReferenceToSelf()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            Object matrix2 = matrix1;
+
+            Assert.That(matrix1.Equals(matrix2), Is.True);
+        }
+
+        [Test]
+        public void Equals_ReturnsTrue_WhenObjectContainsSamesValuesInSamePositions()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            Object matrix2 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+
+            Assert.That(matrix1.Equals(matrix2), Is.True);
+        }
+
+        [Test]
+        public void GetHashCode_ReturnsTrue_WhenMatricesAreSame()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+            var matrix2 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f },
+                    { 3f, 4f, 5f } });
+
+
+            Assert.That(matrix1.GetHashCode(), Is.EqualTo(matrix2.GetHashCode()));
+        }
+
+        [Test]
+        public void Multiply_Returns_InstanceOfMatrix()
+        {
+            var matrix1 = new TMatrix(new Single[4, 4]);
+            var matrix2 = new TMatrix(new Single[4, 4]);
+
+
+            Assert.That(matrix1 * matrix2, Is.InstanceOf<TMatrix>());
+        }
+
+        [Test]
+        public void Multiply_ReturnsMatrix_With4Rows()
+        {
+            var matrix1 = new TMatrix(new Single[4, 4]);
+            var matrix2 = new TMatrix(new Single[4, 4]);
+
+            TMatrix result = matrix1 * matrix2;
+
+            Assert.That(result.Rows, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Multiply_ReturnsMatrix_With4Columns()
+        {
+            var matrix1 = new TMatrix(new Single[4, 4]);
+            var matrix2 = new TMatrix(new Single[4, 4]);
+
+            TMatrix result = matrix1 * matrix2;
+
+            Assert.That(result.Columns, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Multiply_Returns_DotProductForEachCell()
+        {
+            var matrix1 = new TMatrix(
+                new[,] {
+                    { 1f, 2f, 3f, 4f },
+                    { 5f, 6f, 7f, 8f },
+                    { 9f, 8f, 7f, 6f },
+                    { 5f, 4f, 3f, 2f }
+                });
+            var matrix2 = new TMatrix(
+                new[,] {
+                    { -2f, 1f, 2f,  3f },
+                    {  3f, 2f, 1f, -1f },
+                    {  4f, 3f, 6f,  5f },
+                    {  1f, 2f, 7f,  8f }
+                });
+
+            TMatrix result = matrix1 * matrix2;
+
+            var expectedResult = new TMatrix(
+                new[,] {
+                    { 20f, 22f,  50f,  48f },
+                    { 44f, 54f, 114f, 108f },
+                    { 40f, 58f, 110f, 102f },
+                    { 16f, 26f,  46f,  42f }
+                });
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ libTracer/TMatrix.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace libTracer
+{
+    public class TMatrix : IEquatable<TMatrix>
+    {
+        private const Int32 DIMINSION_ROWS = 0;
+        private const Int32 DIMINSION_COLS = 1;
+
+        private readonly Single[,] _members;
+        public Int32 Rows => _members.GetLength(DIMINSION_ROWS);
+
+        public Int32 Columns => _members.GetLength(DIMINSION_COLS);
+
+        public Single this[Int32 col, Int32 row] => _members[row, col];
+
+        public TMatrix(Single[,] members)
+        {
+            _members = members;
+        }
+
+        public static TMatrix operator *(TMatrix m1, TMatrix m2)
+        {
+            // Hard coding for 4x4 matrix
+            return new TMatrix(new[,]
+            {
+                {
+                   //  C, R       C, R       C, R       C, R       C, R       C, R       C, R       C, R
+                    m1[0, 0] * m2[0, 0] + m1[1, 0] * m2[0, 1] + m1[2, 0] * m2[0, 2] + m1[3, 0] * m2[0, 3], // R0 C0
+                    m1[0, 0] * m2[1, 0] + m1[1, 0] * m2[1, 1] + m1[2, 0] * m2[1, 2] + m1[3, 0] * m2[1, 3], // R0 C1
+                    m1[0, 0] * m2[2, 0] + m1[1, 0] * m2[2, 1] + m1[2, 0] * m2[2, 2] + m1[3, 0] * m2[2, 3], // R0 C2
+                    m1[0, 0] * m2[3, 0] + m1[1, 0] * m2[3, 1] + m1[2, 0] * m2[3, 2] + m1[3, 0] * m2[3, 3]  // R0 C3
+                },
+                {
+                    m1[0, 1] * m2[0, 0] + m1[1, 1] * m2[0, 1] + m1[2, 1] * m2[0, 2] + m1[3, 1] * m2[0, 3], // R1 C0
+                    m1[0, 1] * m2[1, 0] + m1[1, 1] * m2[1, 1] + m1[2, 1] * m2[1, 2] + m1[3, 1] * m2[1, 3], // R1 C1
+                    m1[0, 1] * m2[2, 0] + m1[1, 1] * m2[2, 1] + m1[2, 1] * m2[2, 2] + m1[3, 1] * m2[2, 3], // R1 C2
+                    m1[0, 1] * m2[3, 0] + m1[1, 1] * m2[3, 1] + m1[2, 1] * m2[3, 2] + m1[3, 1] * m2[3, 3]  // R1 C3
+                },
+                {
+                    m1[0, 2] * m2[0, 0] + m1[1, 2] * m2[0, 1] + m1[2, 2] * m2[0, 2] + m1[3, 2] * m2[0, 3], // R2 C0
+                    m1[0, 2] * m2[1, 0] + m1[1, 2] * m2[1, 1] + m1[2, 2] * m2[1, 2] + m1[3, 2] * m2[1, 3], // R2 C1
+                    m1[0, 2] * m2[2, 0] + m1[1, 2] * m2[2, 1] + m1[2, 2] * m2[2, 2] + m1[3, 2] * m2[2, 3], // R2 C2
+                    m1[0, 2] * m2[3, 0] + m1[1, 2] * m2[3, 1] + m1[2, 2] * m2[3, 2] + m1[3, 2] * m2[3, 3]  // R2 C3
+                },
+                {
+                    m1[0, 3] * m2[0, 0] + m1[1, 3] * m2[0, 1] + m1[2, 3] * m2[0, 2] + m1[3, 3] * m2[0, 3], // R3 C0
+                    m1[0, 3] * m2[1, 0] + m1[1, 3] * m2[1, 1] + m1[2, 3] * m2[1, 2] + m1[3, 3] * m2[1, 3], // R3 C1
+                    m1[0, 3] * m2[2, 0] + m1[1, 3] * m2[2, 1] + m1[2, 3] * m2[2, 2] + m1[3, 3] * m2[2, 3], // R3 C2
+                    m1[0, 3] * m2[3, 0] + m1[1, 3] * m2[3, 1] + m1[2, 3] * m2[3, 2] + m1[3, 3] * m2[3, 3]  // R3 C3
+                },
+            });
+        }
+
+        public Boolean Equals(TMatrix other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if ((Rows != other.Rows) ||
+                (Columns != other.Columns)) return false;
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    if (Math.Abs(_members[row, col] - other._members[row, col]) > 0.001) return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override Boolean Equals(Object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return Equals((TMatrix)obj);
+        }
+
+        public override Int32 GetHashCode()
+        {
+            Int32 result = HashCode.Combine(Rows, Columns);
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Columns; col++)
+                {
+                    result = HashCode.Combine(_members[row, col], result);
+                }
+            }
+
+            return result;
+        }
+    }
+}
